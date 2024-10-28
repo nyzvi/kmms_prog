e301fdcb8f06aa5807a67021ed3973dc10afe193
nyzvi <twentybuyq@gmail.com>
2024-10-28 15:46:53 +0300

    Update otrez.cpp

--- algosbl/otrez.cpp
+++ algosbl/otrez.cpp
@@ -1,35 +1,29 @@
 #include<iostream>
 #include<vector>
 
-int main()
-{
-    int n;
-    int k;
+int main(){
+    int n,k;
     std::cin >> n >> k;
 
     std::vector <int> mas(n);
-    for (int i = 0; i < n + 1; ++i)
-    {
+    for (int i = 0; i < n + 1; ++i){
         std::cin >> mas[i];
     }
 
     std::vector <int> sp(n);
-    for (int i = 0; i < n; ++i)
-    {
+    for (int i = 0; i < n; ++i){
         sp[i] += 1;
     }
 
     std::vector <int> sp_sum(k);
     sp_sum[0] = sp[0];
-    for (int i = 1; i < k; ++i)
-    {
+    for (int i = 1; i < k; ++i){
         sp_sum[i] = sp_sum[i-1] + sp[i];
     }
 
     int q;
     std:: cin >> q;
-    for (int i = 0; i < q; ++i)
-    {
+    for (int i = 0; i < q; ++i){
         int l, r;
         std:: cin >> l >> r;
         l = std::max(0, l);
