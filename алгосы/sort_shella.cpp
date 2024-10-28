4c5edcf8097a7d2db74802e47be384340b03c51a
nyzvi <twentybuyq@gmail.com>
2024-10-28 19:54:23 +0300

    Update sort_shella.cpp

--- алгосы/sort_shella.cpp
+++ алгосы/sort_shella.cpp
@@ -5,15 +5,15 @@
 #include<vector>
 
 
-void shell_sort (std::vector <int> shell, std::vector <int> random_num, std::vector <int> size, int i){
-    for(int b : shell){
+void shell_sort (std::vector <int> gladkie_shella, std::vector <int> &random_numbers_shella, std::vector <int> size, int i){
+    for(int b : gladkie_shella){
         for (int j = b; j < i; j++) {
-            int temp = random_num[i];
+            int temp = random_numbers_shella[i];
             int n = j;
-            for(;n >= b && random_num[n - b] > temp;n -= b){
-                random_num[n] = random_num[n - b];
+            for(;n >= b && random_numbers_shella[n - b] > temp;n -= b){
+                random_numbers_shella[n] = random_numbers_shella[n - b];
                 }
-            random_num[n] = temp;
+            random_numbers_shella[n] = temp;
         }
     }
 }
@@ -22,6 +22,7 @@ void shell_sort (std::vector <int> shell, std::vector <int> random_num, std::vec
 std::vector <int> gladkie(int n){
     int k = 0;
     std::vector<int> glad;
+    std::vector<int> glad_reverse;
     int pow3 = 1;
     while (pow3 <= n){
         int pow2 = pow3;
@@ -33,7 +34,12 @@ std::vector <int> gladkie(int n){
         pow3 *= 3;
     }
     sort(glad.begin(),glad.end());
-    return glad; 
+    int c = 0;
+    for (int b = 0; b <= k; ++b){
+        glad_reverse[c] = glad[k-c-1];
+        ++c;
+    }
+    return glad_reverse;
 }
 
 
@@ -41,24 +47,33 @@ int main(){
     std::mt19937 mt(10000);
     std::vector <int> size = {1000, 10000, 100000, 1000000};
     for (int i = 0; i < 4; i++){
-        std::vector <int> random_numbers(size[i]);
+        std::vector <int> random_numbers_shella(size[i]);
+        std::vector <int> random_numbers_sort(size[i]);
          for(int j = 0; j < size[i]; j++){
-            random_numbers[j] = mt();
+            random_numbers_shella[j] = mt();
         }
         std::vector <int> gladkie_shella = gladkie(size[i]);
+        random_numbers_sort = random_numbers_shella;
 
 
         auto start_shella = std::chrono::high_resolution_clock::now();
-        shell_sort(gladkie_shella, random_numbers, size, i);
+        shell_sort(gladkie_shella, random_numbers_shella, size, i);
         auto end_shella = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> duration_shella = end_shella - start_shella;
         std::cout << "Время выполнения с помощью сортировки Шелла: " << duration_shella.count() << "\n";
 
 
         auto start_sort = std::chrono::high_resolution_clock::now();
-        std::sort(random_numbers.begin(), random_numbers.end());
+        std::sort(random_numbers_sort.begin(), random_numbers_sort.end());
         auto end_sort = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> duration_sort = end_sort - start_sort;
         std::cout << "Время выполнения с помощью встроенной сортировки: " << duration_sort.count() << "\n";
+        for (int j = 0; j <= size[i]; ++j){
+            if (random_numbers_shella[j] != random_numbers_sort[j]){
+                std:: cout << "Не правильно отсортировано" << "\n";
+                break;
+            }
+        }
+        std::cout << "";
     }
 }
