dbe4bc305606c7c7ab8eb16b396f882d5d5c1359
nyzvi <twentybuyq@gmail.com>
2024-11-25 14:51:38 +0300

    Update Merge_sort.cpp

--- алгосы/Merge_sort.cpp
+++ алгосы/Merge_sort.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 #include<chrono>
 #include<random>
@@ -6,63 +7,13 @@
 
 int global_m = 10;
 
-void merge(std::vector <int> &random_numbers_merge, int left, int mid, int right) {
-    int size_left = mid - left + 1;
-    int size_right = right - mid;
-
-    std::vector <int> L(size_left), R(size_right);
-
-    for (int i = 0; i < size_left; i++)
-        L[i] = random_numbers_merge[left + i];
-    for (int j = 0; j < size_right; j++)
-        R[j] = random_numbers_merge[mid + 1 + j];
-
-    int i = 0, j = 0;
-    int k = left;
-
-    while (i < size_left && j < size_right) {
-        if (L[i] <= R[j]) {
-            random_numbers_merge[k] = L[i];
-            i++;
-        }
-        else {
-            random_numbers_merge[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < size_left) {
-        random_numbers_merge[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < size_right) {
-        random_numbers_merge[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void merge_sort(std::vector<int> &random_numbers_merge, int left, int right) {
-    if (left >= right)
-        return;
-    int mid = left + (right - left) / 2;
-    merge_sort(random_numbers_merge, left, mid);
-    merge_sort(random_numbers_merge, mid + 1, right);
-    merge(random_numbers_merge, left, mid, right);
-}
-
 void vstavka_sort(std::vector <int> &random_numbers_vstavka, int left, int right){
     for (int i = left + 1; i <= right; i++){
-        int key = random_numbers_vstavka[i];
-        int j = i - 1;
-        while (j >= left && random_numbers_vstavka[j] > key){
-            random_numbers_vstavka[j + 1] = random_numbers_vstavka[j];
+        int j = i + 1;
+        while (j >= left && random_numbers_vstavka[j] > random_numbers_vstavka[i]){
+            std::swap(random_numbers_vstavka[j + 1], random_numbers_vstavka[j]);
             j--;
         }
-        random_numbers_vstavka[j + 1] = key;
     }
 }
 
@@ -106,11 +57,9 @@ void merge_vstavka(std::vector<int> &random_numbers_vstavka, int left, int mid, 
 }
 
 void merge_vstavka_sort(std::vector<int> &random_numbers_vstavka, int left, int right) {
-    if (left >= right)
-        return;
-    
     if (right - left + 1 <= global_m){
         vstavka_sort(random_numbers_vstavka, left, right);
+        return;
     } else {    
         int mid = left + (right - left) / 2;
         merge_vstavka_sort(random_numbers_vstavka, left, mid);
@@ -122,36 +71,43 @@ void merge_vstavka_sort(std::vector<int> &random_numbers_vstavka, int left, int 
 int main(){
     std::mt19937 mt(10000);
     std::vector <int> size = {1000, 10000, 100000, 1000000, 1000000};
-
-    for (int i = 0; i < 4; i++){
-        std::vector <int> random_numbers_merge(size[i]);
-        std::vector <int> random_numbers_vstavka(size[i]);
-        for(int j = 0; j < size[i]; j++){
-            random_numbers_merge[j] = mt();
+    for (int i = 0; i < 5; ++i){
+        int n = size[i];
+        std::vector <int> random_numbers_vstavka(n);
+        std::vector <int> clone(n);
+        for (int j = 0; j < n; ++j){
+            random_numbers_vstavka[j] = mt();
         }
-        random_numbers_vstavka = random_numbers_merge;
-        
-        double minim = 1000000000;
-        int maxim = -1000000;
-        for (int m = size[i] / 2; m > 0; --m){
+        random_numbers_vstavka = clone;
+        int good_m = 0;
+
+        double minim = 10000000000;
+        for (int m = 10; m < 200; ++m) {
             global_m = m;
-
-            auto start_merge = std::chrono::high_resolution_clock::now();
-            merge_sort(random_numbers_merge, 0, size[i] - 1);
-            auto end_merge = std::chrono::high_resolution_clock::now();
-            std::chrono::duration <double, std::milli> duration_merge = end_merge - start_merge;
-
+            
             auto start_vstavka = std::chrono::high_resolution_clock::now();
-            merge_vstavka_sort(random_numbers_vstavka, 0, size[i] - 1);
+            merge_vstavka_sort(random_numbers_vstavka, 0, n - 1);
             auto end_vstavka = std::chrono::high_resolution_clock::now();
             std::chrono::duration <double, std::milli> duration_vstavka = end_vstavka - start_vstavka;
+
+            std::sort(clone.begin(), clone.end());
             
-            if (minim > std::abs(duration_vstavka.count() - duration_merge.count())) {
-                minim = std::abs(duration_vstavka.count() - duration_merge.count());
-                maxim = m;
+            std::cout << duration_vstavka.count() << "   " << global_m << std::endl;
+
+            if (minim > duration_vstavka.count()){
+                minim = duration_vstavka.count();
+                good_m = global_m;
             }
         }
-        std::cout << "Минимальная разница во времени достигается при m = " << maxim << ". Если размер масива равен " << size[i] << "\n";
-        std::cout << "\n";
+        std::cout << "Наименьшее время достигается при m = " << good_m << ". Если длина массива равна " << n << std::endl;
+        int k = 0;
+        for(int q = 0; q < n; ++q){
+            if (random_numbers_vstavka[q] == clone[q]){
+                k += 1;
+            }
+            if (k == n){
+                std:: cout << "Массивы отсортированы правильно" << std::endl;
+            }
+        }
     }
 }
