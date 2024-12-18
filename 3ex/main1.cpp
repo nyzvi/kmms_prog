5ba0b73c6ec40e09b0af600ac90c0095a7401b35
nyzvi <twentybuyq@gmail.com>
2024-12-19 00:26:04 +0300

    Create main1.cpp

---
+++ 3ex/main1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "menu.hpp"
+#include "menu_func.hpp"
+#include "menu_items.hpp"
+
+int main () {
+    std::cout << "Привет!" << std::endl;
+    std::cout << "Выбери, что ты хочешь сделать:" << std::endl;
+    
+    const fym::MenuItem* current = &fym::MAIN;
+    do {
+        current = current->func(current);
+    } while (true);
+
+    return 0;
+}
