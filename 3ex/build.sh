ddf35b53ff735cc7f817b6278fc2a9ca51f80af0
nyzvi <twentybuyq@gmail.com>
2024-12-19 08:13:35 +0300

    Update build.sh

--- 3ex/build.sh
+++ 3ex/build.sh
@@ -1,9 +1,13 @@
-MAIN="main.cpp menu_func.cpp menu_items.cpp"
+MAIN="main1.cpp menu_func.cpp menu_items.cpp"
 APP=example
 
-if [ ! -f $APP ]; then rm $APP
+if [ ! -f $"APP" ]; then rm $"APP"
 fi
 
 g++ -std=c++11 $MAIN -o $APP
 
-./$APP
+if [ $? -eq 0 ]; then
+    ./"$APP"
+else
+    echo "Ошибка компиляции."
+fi
